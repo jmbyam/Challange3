1a4ee95b2faa8dc80c8de67cdc83d17977e59030
jmbyam <jmbyam@alaska.edu>
2020-04-18 16:09:53 -0800

    Made a terrain and easter eggs, but still working on changing their color

---
+++ Challenge3/Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material material;
+    Renderer rend;
+
+    void start(){
+        rend = GetComponent<Renderer>();
+    }
+
+    void UpDate(Collision col)
+    {
+        if (col.gameObject.tag == "Player"){
+            rend.material = material;
+        }
+    }
+}
